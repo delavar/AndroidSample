899e3ca066e3c51456781d24a3d04d4045afc979
Amin Mousavi <amn.mousavi@gmail.com>
2025-03-29 15:42:28 +0330

    add sample for providers

--- app/src/main/java/ir/sharif/androidsample/ui/theme/Theme.kt
+++ app/src/main/java/ir/sharif/androidsample/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ir.sharif.androidsample.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +8,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -33,12 +35,45 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+data class AndroidSampleColors(
+    val color1: Color = Color.Black,
+    val color2: Color = Color.Black,
+    val color3: Color = Color.Black,
+)
+
+val LocalColorProvider = staticCompositionLocalOf {
+    AndroidSampleColors()
+}
+
+/**
+ * Access the color shades anywhere in your Composables like this:
+ *
+ * Example usage:
+ * ```
+ * @Composable
+ * fun MyComponent() {
+ *     val colorProvider = LocalColorProvider.current
+ *
+ *     Box(
+ *         modifier = Modifier
+ *             .background(colorProvider.redShades.red400)
+ *             .padding(16.dp)
+ *     ) {
+ *         Text(
+ *             text = "Red Text",
+ *             color = colorProvider.blueShades.blue700
+ *         )
+ *     }
+ * }
+ * ```
+ */
+
 @Composable
 fun AndroidSampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -50,9 +85,17 @@ fun AndroidSampleTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalColorProvider provides AndroidSampleColors(
+            color1 = Red700,
+            color2 = Green700,
+            color3 = Blue700,
+        )
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
